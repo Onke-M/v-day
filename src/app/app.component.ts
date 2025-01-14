252454b44576de34a92e19bd094d88eeba40c5e5
Onke-M <onmweza@gmail.com>
2025-01-14 22:43:47 +0200

    adding initial logic and functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'v-day';
+  showYes = false;
+  showNo = false;
+  showQuestion = false;
+  question: string = '';
+
+  ngOnInit(): void {
+    console.log('app component initialized');
+    this.showQuestion = true;
+    this.question = 'Will you be my Valentine?';
+  }
+
+  showYesButton() {
+    this.showYes = true;
+    this.showNo = false;
+    this.showQuestion = false;
+    this.question = 'WOOOOOOOOOO'
+  }
+
+  showNoButton() {
+    this.showYes = false;
+    this.showNo = true;
+    this.showQuestion = false;
+    this.question = 'So you hate me fr?'
+  }
 }
