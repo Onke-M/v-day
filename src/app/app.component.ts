5438f2f48dd734e68fbf7ad5530ab2a16eeb0d46
Onke-M <onmweza@gmail.com>
2025-01-16 23:36:43 +0200

    adding some styling and more button logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,28 +12,56 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent implements OnInit{
   title = 'v-day';
-  showYes = false;
-  showNo = false;
-  showQuestion = false;
+  showYesPhoto = false;
+  showNoPhoto = false;
+  showQuestionPhoto = false;
+  showYesButton = false;
+  showNoButton = false;
   question: string = '';
+  noCount: number = 0;
+  yesButtonSize = this.noCount * 20 + 16;
 
   ngOnInit(): void {
     console.log('app component initialized');
-    this.showQuestion = true;
+    this.showQuestionPhoto = true;
+    this.showNoButton = true;
+    this.showYesButton = true;
     this.question = 'Will you be my Valentine?';
   }
 
-  showYesButton() {
-    this.showYes = true;
-    this.showNo = false;
-    this.showQuestion = false;
-    this.question = 'WOOOOOOOOOO'
+  getNoButtonText(){
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "What if I asked really nicely?",
+      "Pretty please",
+      "With a cherry on top",
+      "BRO PLEASEEEE",
+      "But :*(",
+      "I am going to die",
+      "You have killed me",
+      "please babe",
+      ":((((",
+      "PRETTY PLEASE",
+      "No :(",
+    ];
+
+    return phrases[Math.min(this.noCount, phrases.length - 1)];
+  };
+
+  handleYes() {
+    this.showYesPhoto = true;
+    this.showQuestionPhoto = false;
+    this.showNoPhoto = false;
+    this.showYesButton = false;
+    this.showNoButton = false;
+    this.question = 'WOOOOOOOOOOOOOOOO'
   }
 
-  showNoButton() {
-    this.showYes = false;
-    this.showNo = true;
-    this.showQuestion = false;
-    this.question = 'So you hate me fr?'
+  handleNo() {
+    this.noCount++;
+    this.yesButtonSize = this.noCount * 20 + 16;
+    this.showNoPhoto = true;
+    this.showQuestionPhoto = false;
   }
 }
